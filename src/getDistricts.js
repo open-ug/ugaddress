d7d59fc22c5c2679fce7848088915a09a746a3ad
Jim Junior <jimjunior854@gmail.com>
2023-05-30 21:06:49 -0700

    Update readme

---
+++ src/getDistricts.js
@@ -0,0 +1,21 @@
+// @ts-check
+
+/** 
+* Get All districts for a specific region
+* @summary If the description is long, write your summary here. Otherwise, feel free to remove this.
+* @param {"Northern" | "Central" | "Western" | "Eastern"} region - Region that you want to get the districts for.
+* @return {Promise<Array>} An Array of districts for the region.
+*/
+export const getDistricts = async (region) => {
+  // check if region is among accepted reions
+  if (["Northern", "Central", "Western", "Eastern"].includes(region)) {
+    // get the districts for the region
+    const response = await fetch(`https://open-ug.github.io/ugaddress/regions/${region}.json`);
+    const data = await response.json();
+    return data;
+  } else {
+    // throw an error
+    throw new Error("Invalid region provided. Region must be one of: Northern, Central, Western, Eastern");
+  }
+
+}
